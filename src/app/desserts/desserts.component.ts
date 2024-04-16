60e2012c040c9e542290e16f1275b317ab4286a8
jorgensra <157417554+jorgensra@users.noreply.github.com>
2024-04-16 16:49:10 -0600

    Initial commit

---
+++ src/app/desserts/desserts.component.ts
@@ -0,0 +1,84 @@
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Dessert } from '../data/dessert';
+import { DessertFilter } from '../data/dessert-filter';
+import { DessertService } from '../data/dessert.service';
+import { DessertIdToRatingMap, RatingService } from '../data/rating.service';
+import { DessertCardComponent } from '../dessert-card/dessert-card.component';
+import { ToastService } from '../shared/toast';
+
+@Component({
+  selector: 'app-desserts',
+  standalone: true,
+  imports: [DessertCardComponent, FormsModule, JsonPipe],
+  templateUrl: './desserts.component.html',
+  styleUrl: './desserts.component.css',
+})
+export class DessertsComponent implements OnInit {
+  #dessertService = inject(DessertService);
+  #ratingService = inject(RatingService);
+  #toastService = inject(ToastService);
+
+  originalName = signal('');
+  englishName = signal('');
+  loading = signal(false);
+
+  desserts = signal<Dessert[]>([]);
+  ratings = signal<DessertIdToRatingMap>({});
+  ratedDesserts = computed(() => this.toRated(this.desserts(), this.ratings()));
+
+  ngOnInit(): void {
+    this.search();
+  }
+
+  search(): void {
+    const filter: DessertFilter = {
+      originalName: this.originalName(),
+      englishName: this.englishName(),
+    };
+
+    this.loading.set(true);
+
+    this.#dessertService.find(filter).subscribe({
+      next: (desserts) => {
+        this.desserts.set(desserts);
+        this.loading.set(false);
+      },
+      error: (error) => {
+        this.loading.set(false);
+        this.#toastService.show('Error loading desserts!');
+        console.error(error);
+      },
+    });
+  }
+
+  toRated(desserts: Dessert[], ratings: DessertIdToRatingMap): Dessert[] {
+    return desserts.map((d) =>
+      ratings[d.id] ? { ...d, rating: ratings[d.id] } : d,
+    );
+  }
+
+  loadRatings(): void {
+    this.loading.set(true);
+
+    this.#ratingService.loadExpertRatings().subscribe({
+      next: (ratings) => {
+        this.ratings.set(ratings);
+        this.loading.set(false);
+      },
+      error: (error) => {
+        this.#toastService.show('Error loading ratings!');
+        console.error(error);
+        this.loading.set(false);
+      },
+    });
+  }
+
+  updateRating(id: number, rating: number): void {
+    this.ratings.update((ratings) => ({
+      ...ratings,
+      [id]: rating,
+    }));
+  }
+}
